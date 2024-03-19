3b5a32d6a02ba058899471faaffd57f6d1dfd7fd
Ronald <ronaldmacario13@gmail.com>
2024-03-19 20:17:18 -0300

    Add SafeMath library and using statement

--- Libraries.sol
+++ Libraries.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.24;
 
 // Importing the SafeMath library
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-}
+
 // Simple contract that uses SafeMath for basic arithmetic
 contract Library {
+    
+    // Using SafeMath for uint256 data type
+    using SafeMath for uint256;
+}
