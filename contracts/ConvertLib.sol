35f7bff3c905fd5f644cc9ec2723c3cc7e28a169
Ronald <ronaldmacario13@gmail.com>
2024-04-05 05:12:43 -0300

    Add .env file and remove unnecessary contracts and tests

--- contracts/ConvertLib.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.24;
-
-library ConvertLib{
-	function convert(uint amount,uint conversionRate) public pure returns (uint convertedAmount)
-	{
-		return amount * conversionRate;
-	}
-}
