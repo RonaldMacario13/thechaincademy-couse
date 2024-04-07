fc1cfef75cd693f145d5faaa3fc63bb3b0a888aa
Ronald <ronaldmacario13@gmail.com>
2024-04-07 20:00:42 -0300

    commenting Hardhat configuration for Sepolia testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,24 @@
+// Require the Hardhat Toolbox library
 require("@nomicfoundation/hardhat-toolbox");
 
-
+// MetaMask private key
 metamask_private_key = "0x" + "6d1dccf82564fd00d99f862beed060619e9a2d887b7e6729ae8745c72d87b05b"
 
-/** @type import('hardhat/config').HardhatUserConfig */
+/** 
+ * Export the Hardhat configuration
+ * @type import('hardhat/config').HardhatUserConfig 
+ */
 module.exports = {
+  // Solidity version
   solidity: "0.8.24",
   
+  // Networks configuration
   networks: {
+    // Sepolia testnet configuration
     sepolia: {
+      // Sepolia testnet URL
       url: `https://sepolia.infura.io/v3/f77f71e6b66a431bab5fd0ee29da334f`,
+      // Accounts associated with Sepolia network (in this case, only one account, MetaMask's private key)
       accounts: [metamask_private_key]
     }
   }
